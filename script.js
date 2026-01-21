c2bab0a3a37b7ff0cb5a5f317e2d34615c2cc4dd
XyPMA9900 <arsenijkrisan52@gmail.com>
2026-01-21 19:43:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ let score = 0;
 
 const scoreEl = document.getElementById("score");
 const catBtn = document.getElementById("cat");
+const resetBtn = document.getElementById("reset");
 
+// загрузка
+if (localStorage.getItem("kotokliker_save")) {
+  score = parseInt(localStorage.getItem("kotokliker_save"));
+  scoreEl.textContent = score;
+}
+
+// клик
 catBtn.onclick = () => {
   score++;
   scoreEl.textContent = score;
+  localStorage.setItem("kotokliker_save", score);
 };
 
-// анти-двойной зум
+// сброс
+resetBtn.onclick = () => {
+  if (confirm("Точно сбросить прогресс?")) {
+    score = 0;
+    scoreEl.textContent = score;
+    localStorage.removeItem("kotokliker_save");
+  }
+};
+
+// анти-зум
 document.addEventListener("dblclick", e => {
   e.preventDefault();
 });
