8e679e25f99946822041a328314d2c15ffd62160
XyPMA9900 <arsenijkrisan52@gmail.com>
2026-01-25 19:15:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ let score = 0;
 let scoreText = document.getElementById("score");
 let cat = document.getElementById("cat");
 
+let shop = document.getElementById("shop");
+let openShop = document.getElementById("openShop");
+let closeShop = document.getElementById("closeShop");
+
+/* КЛИК */
 cat.onclick = () => {
   score += 1;
   scoreText.textContent = score + " 🐟";
+};
+
+/* МОДАЛКА */
+openShop.onclick = () => {
+  shop.classList.add("show");
+};
+
+closeShop.onclick = () => {
+  shop.classList.remove("show");
 };
