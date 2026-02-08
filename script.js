4e530282be8f7cbedacb4418d5d45d938c141101
XyPMA9900 <arsenijkrisan52@gmail.com>
2026-02-08 19:13:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = () => {
-
 const $ = id => document.getElementById(id);
 
 /* ===== GAME ===== */
@@ -135,18 +133,13 @@ function save(){
   }));
 }
 
-let score = 0;
-let clickPower = 1;
-let autoPower = 0;
-
 function load(){
   let d = JSON.parse(localStorage.getItem("save"));
   if(!d) return;
-
-  score = d.score;
-  clickPower = d.clickPower;
-  autoPower = d.autoPower;
-  d.items.forEach((c,i)=>items[i].count = c);
+  score=d.score;
+  clickPower=d.clickPower;
+  autoPower=d.autoPower;
+  d.items.forEach((c,i)=>items[i].count=c);
 }
 
 /* ===== UI ===== */
@@ -240,5 +233,3 @@ $("closeShop").onclick=()=>$("shop").classList.remove("show");
 /* ===== START ===== */
 load();
 update();
-
-};
